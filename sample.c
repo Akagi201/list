30532982f5bcea6eaedc4dffab74b65287d5dbf2
Akagi201 <akagi201@gmail.com>
2015-03-18 17:42:24 +0800

    add missing kernel defines to list.h

---
+++ sample.c
@@ -0,0 +1,16 @@
+/*
+ * @file sample.c
+ * @author Akagi201
+ * @date 2015/03/18
+ *
+ * sample for list.h
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+int main() {
+    return 0;
+}
